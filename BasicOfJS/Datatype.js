2ff12d4808f9606bf85e4dff9941cc8aa5372440
chaubeyjaya <jayachaubey03@gmail.com>
2025-12-04 22:09:42 +0530

    javascripdatatype and variavle learn

---
+++ BasicOfJS/Datatype.js
@@ -0,0 +1,24 @@
+var age  = 23;
+var name = "jaya";
+ var istrue = true;
+console.log(age)
+console.log(typeof(age))
+console.log(name)
+console.log(typeof(name))
+console.log(istrue)
+console.log(typeof(isture))
+
+
+//object :it is collection of different variable
+const student ={
+    name:"jaya",
+    age:23,
+    cgpa:9.0
+}
+student["age"]=28 //update the value of object
+console.log(student["name"])
+console.log(student.age)
+
+//array
+let colors = ["red", "green", "blue"];
+console.log(colors);
---
+++ BasicOfJS/variable.js
@@ -0,0 +1,40 @@
+// console.log("hello jaya");
+// let age = 20;
+// if(age>20){
+//     console.log("you can drive");
+// }else{
+//     console.log("you can not drivr")
+// }
+
+
+var x = 10;
+console.log(x);  // 10
+
+var x = 20;      // re-declared (allowed)
+console.log(x);  // 20
+
+x = 30;          // updated (allowed)
+console.log(x);  // 30
+
+
+// let example
+let y = 5;
+console.log(y);  // 5
+
+// let y = 10;   ❌ NOT allowed (re-declaration error)
+
+y = 10;          // ✔ allowed (update)
+console.log(y);  // 10
+
+
+// const example
+const z = 50;
+console.log(z);   // 50
+
+// z = 60;       ❌ NOT allowed (update error)
+// const z = 70; ❌ NOT allowed (re-declare error)
+
+
+let name = "jaya";
+console.log(name)
+console.log(typeof(name))
